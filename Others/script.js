5ae70416d39248dbe7075afc4dfb3824f5f4a5fd
aliyuade002 <aliyuade002@gmail.com>
2024-07-17 09:12:50 +0100

    Testing

---
+++ Others/script.js
@@ -0,0 +1,13 @@
+let firstName = prompt('Please enter your Firstname: ')
+let lastName = prompt('Please enter your Lastname: ')
+let age = parseInt(prompt('Please enter your Age: ')) 
+age = age + 2
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+  
+
+// function Display() {
+//    document.getElementById("message").innerHtml = message-content
+//    console.log(message)
+// }
